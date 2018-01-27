c364e1bca8dd90c464149baab9a3bd60f5adc522
malikumer <lablnet01@gmail.com>
2018-01-27 18:04:14 +0500

    initial:

---
+++ example.php
@@ -0,0 +1,34 @@
+<?php 
+
+require_once 'classes/Zip.php';
+
+$zip = new Zip;
+
+/**
+//Note if you want manipulate files in outside the root of server / script you may use my file manipulation class here is two possible link
+Github: https://github.com/Lablnet/PHP-files-manipulation-class
+Alternative: https://www.phpclasses.org/package/10621-PHP-Manipulate-files-and-directories-in-several-ways.html
+please fork/rate these classes if you like thanks
+*/
+//compress file
+$comp = $zip->Compress(
+['data/image/one.png','data/image/two.png'],
+'data/compressed/images.zip',false
+);
+if($comp === true){
+	echo "file successfully compressed ";
+}else{
+	echo "already / something went wrong ";
+}
+
+
+
+//extract file
+$ext = $zip->Extract(
+	'data/compressed/images.zip','data/extract/'
+);
+if($ext === true){
+	echo " file extract successfully ";
+}else{
+	echo " already / something went wrong ";
+}
---
+++ readme.php
@@ -0,0 +1,6 @@
+# PHP-zip-handler-class
+## This package can Handel zip/archives.
+
+It can perform
+ 1. Extract files from zip
+ 2. Compressed files into zip
