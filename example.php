7406ddc4f70fa9b387bca8c5e8042a4acbbafb9e
Malik Umer Farooq <Lablnet01@gmail.com>
2018-07-26 06:50:51 +0000

    Apply fixes from StyleCI

--- example.php
+++ example.php
@@ -1,34 +1,33 @@
-<?php 
+<?php
+
 
 require_once 'classes/Zip.php';
 
-$zip = new Zip;
+$zip = new Zip();
 
 /**
-//Note if you want manipulate files in outside the root of server / script you may use my file manipulation class here is two possible link
-Github: https://github.com/Lablnet/PHP-files-manipulation-class
-Alternative: https://www.phpclasses.org/package/10621-PHP-Manipulate-files-and-directories-in-several-ways.html
-please fork/rate these classes if you like thanks
-*/
+ * //Note if you want manipulate files in outside the root of server / script you may use my file manipulation class here is two possible link
+ * Github: https://github.com/Lablnet/PHP-files-manipulation-class
+ * Alternative: https://www.phpclasses.org/package/10621-PHP-Manipulate-files-and-directories-in-several-ways.html
+ * please fork/rate these classes if you like thanks.
+ */
 //compress file
 $comp = $zip->Compress(
-['data/image/one.png','data/image/two.png'],
-'data/compressed/images.zip',false
+['data/image/one.png', 'data/image/two.png'],
+'data/compressed/images.zip', false
 );
-if($comp === true){
-	echo "file successfully compressed ";
-}else{
-	echo "already / something went wrong ";
+if ($comp === true) {
+    echo 'file successfully compressed ';
+} else {
+    echo 'already / something went wrong ';
 }
-
-
 
 //extract file
 $ext = $zip->Extract(
-	'data/compressed/images.zip','data/extract/'
+    'data/compressed/images.zip', 'data/extract/'
 );
-if($ext === true){
-	echo " file extract successfully ";
-}else{
-	echo " already / something went wrong ";
+if ($ext === true) {
+    echo ' file extract successfully ';
+} else {
+    echo ' already / something went wrong ';
 }
